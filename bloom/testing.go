abce45e506b91e503c6790ec45725d5c800fb699
sammy00 <xiangminli@outlook.com>
2018-11-29 20:26:51 +0800

    add testing for Filter.AddOutPoint

--- bloom/testing.go
+++ bloom/testing.go
@@ -2,9 +2,25 @@ package bloom
 
 import (
 	"encoding/hex"
+
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+
+	btcwire "github.com/btcsuite/btcd/wire"
 )
 
 func Hexlify(str string) []byte {
+	out, _ := hex.DecodeString(str)
+	return out
+}
+
+func NewOutPoint(hash []byte, index uint32) *btcwire.OutPoint {
+	var chash chainhash.Hash
+	copy(chash[:], hash)
+
+	return btcwire.NewOutPoint(&chash, index)
+}
+
+func Unhexlify(str string) []byte {
 	out, _ := hex.DecodeString(str)
 	return out
 }
